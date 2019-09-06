41fbcaedbb5a0f1bd3cdcf6e52d30c50e9f4bde5
Ian Lew <962000104@qq.com>
2019-09-06 17:57:15 +0800

    按需加载

--- src/main.js
+++ src/main.js
@@ -2,10 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Vant from 'vant'
-import 'vant/lib/index.css'
+import { Tabbar, TabbarItem } from 'vant'
 
-Vue.use(Vant)
+Vue.use(Tabbar).use(TabbarItem)
 
 Vue.config.productionTip = false
 
