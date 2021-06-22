79fc7c1aae23240782b1886de6efe0d9714a6986
liuyang <962000104@qq.com>
2021-06-22 16:25:09 +0800

    update the overall layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,27 @@
 <template>
   <a-layout class="home">
-    <a-layout-sider v-model:collapsed="isCollapsed" :trigger="null" collapsible>
+    <a-layout-sider
+      v-model:collapsed="isCollapsed"
+      :trigger="null"
+      width="160"
+      collapsible>
       <div class="logo">
         <img src="@/assets/img/logo.png" alt="">
       </div>
+      <a-menu v-model:selectedKeys="selectedKeys" mode="inline" theme="dark">
+        <a-menu-item key="1">
+          <pie-chart-outlined />
+          <span>Option 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <desktop-outlined />
+          <span>Option 2</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <inbox-outlined />
+          <span>Option 3</span>
+        </a-menu-item>
+      </a-menu>
     </a-layout-sider>
     <a-layout>
       <a-layout-header>
@@ -13,6 +31,44 @@
               <a-col>
                 <menu-unfold-outlined v-if="isCollapsed" class="collapsed-trigger" @click="() => (isCollapsed = !isCollapsed)" />
                 <menu-fold-outlined v-else class="collapsed-trigger" @click="() => (isCollapsed = !isCollapsed)" />
+              </a-col>
+              <a-col>
+                <a-breadcrumb>
+                  <a-breadcrumb-item>Home</a-breadcrumb-item>
+                  <a-breadcrumb-item>Option 1</a-breadcrumb-item>
+                </a-breadcrumb>
+              </a-col>
+            </a-row>
+          </a-col>
+          <a-col>
+            <a-row type="flex" justify="center">
+              <a-col>
+                <a-dropdown trigger="hover">
+                  <template #overlay>
+                    <a-menu model="inline" class="setting-menu">
+                      <a-menu-item>
+                        <user-outlined />
+                        <span>个人信息</span>
+                      </a-menu-item>
+                      <a-menu-item>
+                        <setting-outlined />
+                        <span>系统设置</span>
+                      </a-menu-item>
+                      <a-menu-item>
+                        <export-outlined />
+                        <span>退出登录</span>
+                      </a-menu-item>
+                    </a-menu>
+                  </template>
+                  <div class="user">
+                    <a-avatar src="">
+                      <template #icon>
+                        <user-outlined />
+                      </template>
+                    </a-avatar>
+                    <span>administrator</span>
+                  </div>
+                </a-dropdown>
               </a-col>
             </a-row>
           </a-col>
@@ -32,22 +88,28 @@ export default defineComponent({
   name: 'home',
   setup() {
     const isCollapsed = ref(false)
+    const selectedKeys = ref([])
 
     return {
-      isCollapsed
+      isCollapsed,
+      selectedKeys
     }
   }
 })
 </script>
 
 <style lang="less" scoped>
+// 最新版菜单收缩后会显示图标
+:global(.ant-tooltip .anticon) {
+  display: none;
+}
 .home {
   height: inherit;
 }
 .logo {
-  height: 60px;
+  height: 32px;
   margin: 16px;
-  & > img {
+  &>img {
     width: 100%;
     height: 100%;
     object-fit: contain;
@@ -57,17 +119,36 @@ export default defineComponent({
   background: #fff;
   padding: 0;
 }
+.setting-menu {
+  text-align: center;
+}
 .ant-row-space-between {
-  & > .ant-col > .ant-row > .ant-col {
+  &>.ant-col>.ant-row>.ant-col {
+    cursor: pointer;
     padding-left: 16px;
     padding-right: 16px;
   }
 }
 .collapsed-trigger {
   font-size: 20px;
-  cursor: pointer;
   &:hover {
     color: @color-primary;
+  }
+}
+.collapsed-trigger,
+.ant-breadcrumb {
+  display: inline-block;
+  vertical-align: middle;
+}
+.user {
+  &>span {
+    display: inline-block;
+    vertical-align: middle;
+  }
+  .ant-avatar+span {
+    max-width: 80px;
+    margin-left: 5px;
+    .ellipsis();
   }
 }
 </style>
